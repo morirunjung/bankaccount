29e1d1a0425238f4f6373e154dcee4455ada5146
AOF-TH <morirunjung@gmail.com>
2020-08-28 11:01:11 +0700

    first commit

---
+++ src/main/java/th/go/rd/bankaccount/controller/BankAccountRestController.java
@@ -0,0 +1,35 @@
+package th.go.rd.bankaccount.controller;
+
+import org.springframework.web.bind.annotation.*;
+import th.go.rd.bankaccount.data.BankAccountRepository;
+import th.go.rd.bankaccount.model.BankAccount;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bankaccount")
+public class BankAccountRestController {
+
+    private BankAccountRepository repository;
+
+    public BankAccountRestController(BankAccountRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<BankAccount> getAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public BankAccount getOne(@PathVariable int id) {
+        return repository.findById(id).get();
+    }
+
+    @PostMapping
+    public BankAccount create(@RequestBody BankAccount bankAccount){
+        repository.save(bankAccount);
+        return repository.findById(bankAccount.getId()).get();
+    }
+
+}
